ac67fc3be38033b4c9ab2dc13bf2309f8a8698d5
Kenneth Murerwa <xwaxes@gmail.com>
2023-12-06 08:33:42 +0300

    feat: Complete AOC Day 5

--- src/main/kotlin/year2023/day5/Part1.kt
+++ src/main/kotlin/year2023/day5/Part1.kt
@@ -1,5 +1,76 @@
 package year2023.day5
 
-fun main() {
+import utils.InputGetter
+import kotlin.collections.HashMap
 
+fun main() {
+    val inputArr = InputGetter.getFileInput(5, 2023, "input")
+
+    val processedPair = processDay5Part1Input(inputArr)
+
+    val seeds = processedPair.first
+    var min = Long.MAX_VALUE
+    for (i in seeds) {
+        val location = walk(i, processedPair.second)
+        min = min.coerceAtMost(location)
+    }
+
+    println("Min: $min")
+}
+
+fun walk(seed: Long, map: HashMap<String, MutableList<List<Long>>>): Long {
+    val listOfWalk = listOf(
+        "seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature",
+        "temperature-to-humidity", "humidity-to-location"
+    )
+
+    val from = 0
+    val to = 1
+    val range = 2
+
+    var curr = seed
+
+    for (key in listOfWalk) {
+        val values = map[key]!!
+        for (valueList in values) {
+            val end = valueList[to] + valueList[range]
+            if (curr >= valueList[to] && curr < end) {
+                val ratio = valueList[from] - valueList[to]
+                curr += ratio
+                break
+            }
+        }
+    }
+
+    return curr
+}
+
+fun processDay5Part1Input(inputArr: List<String>): Pair<List<Long>, HashMap<String, MutableList<List<Long>>>> {
+    val filtered = inputArr.filter { s -> s.isNotEmpty() }
+
+    var seeds: List<Long> = listOf()
+    var key = ""
+    val mapOfList: HashMap<String, MutableList<List<Long>>> = HashMap()
+    filtered.forEachIndexed { index, str ->
+        if (index == 0) {
+            val seedSplit = str.split(": ")
+            seeds = seedSplit[1].split(" ").map { it.toLong() }
+        } else {
+            if (str.contains(":")) {
+                key = str.split(" ")[0]
+            } else {
+                val list = mapOfList.getOrDefault(key, mutableListOf())
+                list.add(str.split(" ").map { it.toLong() })
+                mapOfList[key] = list
+            }
+        }
+    }
+
+
+    for (entry in mapOfList) {
+        println(entry)
+    }
+    println("\n")
+
+    return Pair(seeds, mapOfList)
 }
--- src/main/kotlin/year2023/day5/Part2.kt
+++ src/main/kotlin/year2023/day5/Part2.kt
@@ -1,5 +1,51 @@
 package year2023.day5
 
-fun main() {
+import utils.InputGetter
+import kotlin.collections.HashMap
 
+fun main() {
+    val inputArr = InputGetter.getFileInput(5, 2023)
+
+    val processedPair = processDay5Part2Input(inputArr)
+
+    val seeds = processedPair.first
+    var min = Long.MAX_VALUE
+    for (pair in seeds) {
+        val end = (pair.first + pair.second) - 1
+        for (seed in pair.first..end) {
+            val location = walk(seed, processedPair.second)
+            min = min.coerceAtMost(location)
+        }
+    }
+
+    println("Min: $min")
+}
+
+fun processDay5Part2Input(inputArr: List<String>): Pair<List<Pair<Long, Long>>, HashMap<String, MutableList<List<Long>>>> {
+    val filtered = inputArr.filter { s -> s.isNotEmpty() }
+
+    val seeds: MutableList<Pair<Long, Long>> = mutableListOf()
+    var key = ""
+    val mapOfList: HashMap<String, MutableList<List<Long>>> = HashMap()
+    filtered.forEachIndexed { index, str ->
+        if (index == 0) {
+            val seedSplit = str.split(": ")
+            val seedList = seedSplit[1].split(" ").map { it.toLong() }
+            var ind = 0
+            while (ind < seedList.size - 1) {
+                seeds.add(Pair(seedList[ind], seedList[ind + 1]))
+                ind += 2
+            }
+        } else {
+            if (str.contains(":")) {
+                key = str.split(" ")[0]
+            } else {
+                val list = mapOfList.getOrDefault(key, mutableListOf())
+                list.add(str.split(" ").map { it.toLong() })
+                mapOfList[key] = list
+            }
+        }
+    }
+
+    return Pair(seeds, mapOfList)
 }
