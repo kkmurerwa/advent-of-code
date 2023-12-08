2a4b4079eabd55749642dbb9c66f063a2c651e18
Kenneth Murerwa <xwaxes@gmail.com>
2023-12-08 08:52:06 +0300

    feat: Add solution for AOC day 8

--- src/main/kotlin/year2023/day8/Part1.kt
+++ src/main/kotlin/year2023/day8/Part1.kt
@@ -1,5 +1,58 @@
 package year2023.day8
 
-fun main() {
+import utils.InputGetter
 
+fun main() {
+    val res = navigateToEnd("AAA")
+
+    println("Res: $res")
+}
+
+fun processDay8Input(): Pair<String, MutableMap<String, Pair<String, String>>> {
+    val inputArr = InputGetter.getFileInput(8, 2023, "input")
+    var instructions = ""
+
+    val map = mutableMapOf<String, Pair<String, String>>()
+
+    inputArr.forEachIndexed { index, str ->
+        if (index == 0) {
+            instructions = str
+        } else if (str.isNotEmpty()) {
+            val tempArr = str.split(" = ")
+            val options = tempArr[1].replace("(", "")
+                .replace(")","").split(",").map { it.trim() }
+
+            map[tempArr[0]] = Pair(options[0], options[1])
+        }
+    }
+
+    return Pair(instructions, map)
+}
+
+fun navigateToEnd(start: String): Int {
+    var curr = start
+    val (instructions, map) = processDay8Input()
+    val endingPoints = map.keys.filter { it[2] == 'Z' }.toSet()
+
+    var reachedEnd = false
+    var res = 0
+    while (!reachedEnd) {
+        for (char in instructions) {
+            if (endingPoints.contains(curr)) {
+                reachedEnd = true
+                break
+            }
+
+            val pair =  map.getValue(curr)
+            curr = if (char == 'L') {
+                pair.first
+            } else {
+                pair.second
+            }
+
+            res += 1
+        }
+    }
+
+    return res
 }
--- src/main/kotlin/year2023/day8/Part2.kt
+++ src/main/kotlin/year2023/day8/Part2.kt
@@ -1,5 +1,47 @@
 package year2023.day8
 
 fun main() {
+    val (_, map) = processDay8Input()
 
+    val startingPoints = map.keys.filter { it[2] == 'A' }
+    val movesTaken = mutableMapOf<String, Int>()
+
+    startingPoints.forEach { str ->
+        movesTaken[str] = navigateToEnd(str)
+    }
+
+    val res = lcmOfArrayElements(movesTaken.values.toMutableList())
+
+    println("Res: $res")
+}
+
+fun lcmOfArrayElements(elementArray: MutableList<Int>): Long {
+    var lcmOfArrayElements: Long = 1
+    var divisor = 2
+    while (true) {
+        var counter = 0
+        var divisible = false
+        for (i in elementArray.indices) {
+            if (elementArray[i] == 0) {
+                return 0
+            } else if (elementArray[i] < 0) {
+                elementArray[i] = elementArray[i] * -1
+            }
+            if (elementArray[i] == 1) {
+                counter++
+            }
+            if (elementArray[i] % divisor == 0) {
+                divisible = true
+                elementArray[i] = elementArray[i] / divisor
+            }
+        }
+        if (divisible) {
+            lcmOfArrayElements *= divisor
+        } else {
+            divisor++
+        }
+        if (counter == elementArray.size) {
+            return lcmOfArrayElements
+        }
+    }
 }
